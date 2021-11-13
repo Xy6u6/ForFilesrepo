6b7dc3fb799771ea65176f129b55dca1b93afddb
Anton.Voroniuk <anton.voroniuk@gmail.com>
2021-11-14 00:01:59 +0200

    add str handler

--- test.py
+++ test.py
@@ -1,7 +1,12 @@
 def multiply(a, b):
     return a * b
 
+while True:
+    try:
+            a = int(input("enter a"))
+            b = int(input("enter b"))
+            print(multiply(a, b))
+            break
+    except ValueError:
+        print("Please enter the number")
 
-a = int(input("enter a"))
-b = int(input("enter b"))
-print(multiply(a,b))
