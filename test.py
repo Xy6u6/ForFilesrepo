3bc8509a2209b8260c1d4df6620aae3eaae90453
Anton.Voroniuk <anton.voroniuk@gmail.com>
2021-11-14 00:10:59 +0200

    add divide function

--- test.py
+++ test.py
@@ -2,11 +2,15 @@
 def multiply(a, b):
     return a * b
 
+def divide(a, b):
+    return a//b
+
 while True:
     try:
             a = int(input("enter a"))
             b = int(input("enter b"))
             print(multiply(a, b))
+            print(divide(a, b))
             break
     except ValueError:
         print("Please enter the number")
